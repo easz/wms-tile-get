f1388150223c071e9ac21114e59a237194ef1315
easz <easy.lin@gmail.com>
2021-05-12 21:47:07 +0200

    use console script

--- setup.py
+++ setup.py
@@ -6,15 +6,19 @@ with codecs_open("README.md", encoding="utf-8") as f:
 
 setup(
     name='wms-tile-get',
-    version='0.2',
+    version='0.3',
     packages=find_packages(exclude=["ez_setup", "example", "test"]),
-    description='Fetch map tiles from WMS server and store them with WMTS convention',
+    description='Fetch map from WMS server and store them as tiled web map with Slippy Map or or Google Map convention',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='http://github.com/easz/wms-tile-get',
     install_requires=[
           'requests', 'supermercado', 'mercantile'
     ],
-    scripts=['scripts/wms-tile-get'],
+    entry_points={
+    'console_scripts': [
+        'wms-tile-get=scripts.wms_tile_get:main',
+    ],
+},
     license='MIT'
 )
